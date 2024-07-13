92e5cd3ef879f0df58e7a31a703495ae237d162a
xelilovkamran <xlilovkamran@gmail.com>
2024-07-13 16:24:53 +0400

    project done

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -7,7 +7,7 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-let conunt = 0;
+let count = 0;
 
 function updateCounter() {
   count++;
